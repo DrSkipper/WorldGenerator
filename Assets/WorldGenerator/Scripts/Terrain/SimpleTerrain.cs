2e491bf2c7fdd9e2c12f5373e8a348ca8e8dbe4e
Fletcher <fletcherc89@gmail.com>
2017-01-26 22:57:15 -0800

    Beginning to mess around with terrain

---
+++ Assets/WorldGenerator/Scripts/Terrain/SimpleTerrain.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class SimpleTerrain : MonoBehaviour
+{
+    public TileRenderer TileRenderer;
+
+    void Start()
+    {
+        this.TileRenderer.CreateEmptyMap(50, 50);
+    }
+}
