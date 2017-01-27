4677ff8811b77d46647c021648e66dba7b02c5f2
Fletcher <fletcherc89@gmail.com>
2017-01-26 23:24:57 -0800

    Messin' with vertices

--- Assets/WorldGenerator/Scripts/Terrain/SimpleTerrain.cs
+++ Assets/WorldGenerator/Scripts/Terrain/SimpleTerrain.cs
@@ -7,5 +7,14 @@ public class SimpleTerrain : MonoBehaviour
     void Start()
     {
         this.TileRenderer.CreateEmptyMap(50, 50);
+
+        Vector3[] v = this.TileRenderer.MeshFilter.mesh.vertices;
+
+        for (int i = 0; i < v.Length; ++i)
+        {
+            v[i] = new Vector3(v[i].x, v[i].y, v[i].z - (int)(i / 50));
+        }
+
+        this.TileRenderer.MeshFilter.mesh.vertices = v;
     }
 }
