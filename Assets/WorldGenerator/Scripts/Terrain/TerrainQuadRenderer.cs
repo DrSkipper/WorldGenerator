10f23d4cb670317c2156307893e87f1b8fdb262b
Fletcher <fletcherc89@gmail.com>
2017-01-28 14:32:55 -0800

    Higher tiles handle sides, normals should be all good

--- Assets/WorldGenerator/Scripts/Terrain/TerrainQuadRenderer.cs
+++ Assets/WorldGenerator/Scripts/Terrain/TerrainQuadRenderer.cs
@@ -32,12 +32,9 @@ public class TerrainQuadRenderer : MonoBehaviour
 
     private void createTerrainUsingHeightMap(int[,] heightMap)
     {
-        float originX = 0; // this.transform.position.x;
-        float originY = 0; // this.transform.position.y;
-        float originZ = 0; // this.transform.position.z;
-
-        int numTiles = _width * _height;
-        int numTriangles = numTiles * 3 * 2;
+        int originX = 0; // this.transform.position.x;
+        int originY = 0; // this.transform.position.y;
+        int originZ = 0; // this.transform.position.z;
 
         // Generate mesh data
         List<Vector3> vertices = new List<Vector3>();
@@ -56,29 +53,52 @@ public class TerrainQuadRenderer : MonoBehaviour
             for (int x = 0; x < _width; ++x)
             {
                 // Create 4 verts
+                int height = originY + heightMap[x, z] * this.TileRenderSize;
                 float smallZ = originZ + z * this.TileRenderSize;
                 float bigZ = smallZ + this.TileRenderSize;
-                Vector3 bottomLeft = new Vector3(originX + x * this.TileRenderSize, originY + heightMap[x, z] * this.TileRenderSize, smallZ);
-                Vector3 bottomRight = new Vector3(bottomLeft.x + this.TileRenderSize, originY + heightMap[x, z] * this.TileRenderSize, bottomLeft.z);
-                Vector3 topLeft = new Vector3(bottomLeft.x, originY + heightMap[x, z] * this.TileRenderSize, bigZ);
-                Vector3 topRight = new Vector3(bottomRight.x, originY + heightMap[x, z] * this.TileRenderSize, topLeft.z);
+                Vector3 bottomLeft = new Vector3(originX + x * this.TileRenderSize, height, smallZ);
+                Vector3 bottomRight = new Vector3(bottomLeft.x + this.TileRenderSize, height, bottomLeft.z);
+                Vector3 topLeft = new Vector3(bottomLeft.x, height, bigZ);
+                Vector3 topRight = new Vector3(bottomRight.x, height, topLeft.z);
                 addQuad(bottomLeft, bottomRight, topLeft, topRight, bottomLeftUV, bottomRightUV, topLeftUV, topRightUV, Vector3.up, vertices, normals, uvs, triangles);
 
                 // Side tris
-                int rightNeighborHeight = (x + 1 >= _width ? heightMap[x, z] : heightMap[x + 1, z]) * this.TileRenderSize;
-                int upNeighborHeight = (z + 1 >= _height ? heightMap[x, z] : heightMap[x, z + 1]) * this.TileRenderSize;
-                int leftNeighborHeight = (x - 1 < 0 ? heightMap[x, z] : heightMap[x - 1, z]) * this.TileRenderSize;
-                int downNeighborHeight = (z - 1 < 0 ? heightMap[x, z] : heightMap[x, z - 1]) * this.TileRenderSize;
+                int rightNeighborHeight = x + 1 >= _width ? height : (originY + heightMap[x + 1, z] * this.TileRenderSize);
+                int upNeighborHeight = z + 1 >= _height ? height : (originY + heightMap[x, z + 1] * this.TileRenderSize);
+                int leftNeighborHeight = x - 1 < 0 ? height : (originY + heightMap[x - 1, z] * this.TileRenderSize);
+                int downNeighborHeight = z - 1 < 0 ? height : (originY + heightMap[x, z - 1] * this.TileRenderSize);
 
                 // Right side
-                Vector3 rightBottomLeft = new Vector3(bottomRight.x, originY + rightNeighborHeight, bottomRight.z);
-                Vector3 rightBottomRight = new Vector3(topRight.x, originY + rightNeighborHeight, topRight.z);
-                addQuad(rightBottomLeft, rightBottomRight, bottomRight, topRight, bottomLeftUV, bottomRightUV, topLeftUV, topRightUV, Vector3.right, vertices, normals, uvs, triangles);
+                if (rightNeighborHeight < height)
+                {
+                    Vector3 rightBottomLeft = new Vector3(bottomRight.x, rightNeighborHeight, bottomRight.z);
+                    Vector3 rightBottomRight = new Vector3(topRight.x, rightNeighborHeight, topRight.z);
+                    addQuad(rightBottomLeft, rightBottomRight, bottomRight, topRight, bottomLeftUV, bottomRightUV, topLeftUV, topRightUV, Vector3.right, vertices, normals, uvs, triangles);
+                }
 
                 // Up side
-                Vector3 upBottomLeft = new Vector3(topRight.x, originY + upNeighborHeight, topRight.z);
-                Vector3 upBottomRight = new Vector3(topLeft.x, originY + upNeighborHeight, topLeft.z);
-                addQuad(upBottomLeft, upBottomRight, topRight, topLeft, bottomLeftUV, bottomRightUV, topLeftUV, topRightUV, Vector3.forward, vertices, normals, uvs, triangles);
+                if (upNeighborHeight < height)
+                {
+                    Vector3 upBottomLeft = new Vector3(topRight.x, upNeighborHeight, topRight.z);
+                    Vector3 upBottomRight = new Vector3(topLeft.x, upNeighborHeight, topLeft.z);
+                    addQuad(upBottomLeft, upBottomRight, topRight, topLeft, bottomLeftUV, bottomRightUV, topLeftUV, topRightUV, Vector3.forward, vertices, normals, uvs, triangles);
+                }
+
+                // Left side
+                if (leftNeighborHeight < height)
+                {
+                    Vector3 leftBottomLeft = new Vector3(topLeft.x, leftNeighborHeight, topLeft.z);
+                    Vector3 leftBottomRight = new Vector3(bottomLeft.x, leftNeighborHeight, bottomLeft.z);
+                    addQuad(leftBottomLeft, leftBottomRight, topLeft, bottomLeft, bottomLeftUV, bottomRightUV, topLeftUV, topRightUV, Vector3.left, vertices, normals, uvs, triangles);
+                }
+
+                // Down side
+                if (downNeighborHeight < height)
+                {
+                    Vector3 downBottomLeft = new Vector3(bottomLeft.x, downNeighborHeight, bottomLeft.z);
+                    Vector3 downBottomRight = new Vector3(bottomRight.x, downNeighborHeight, bottomRight.z);
+                    addQuad(downBottomLeft, downBottomRight, bottomLeft, bottomRight, bottomLeftUV, bottomRightUV, topLeftUV, topRightUV, Vector3.back, vertices, normals, uvs, triangles);
+                }
             }
         }
 
