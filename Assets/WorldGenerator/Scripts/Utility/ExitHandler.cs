53c82b8b4c4a5d4837f2e34dd73df1bad68b8faa
Fletcher <fletcherc89@gmail.com>
2017-02-13 23:42:35 -0800

    Exit with esc and bigger size

---
+++ Assets/WorldGenerator/Scripts/Utility/ExitHandler.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ExitHandler : VoBehavior
+{
+    public string Destination = "";
+    //public ExitInput Input = ExitInput.Exit;
+
+    [System.Serializable]
+    public enum ExitInput
+    {
+        Exit,
+        Pause,
+        Cancel
+    }
+
+    void Update()
+    {
+        bool pressed = Input.GetKey(KeyCode.Escape);
+        /*switch (this.Input)
+        {
+            default:
+            case ExitInput.Exit:
+                pressed = MenuInput.Exit();
+                break;
+            case ExitInput.Pause:
+                pressed = MenuInput.Pause();
+                break;
+            case ExitInput.Cancel:
+                pressed = MenuInput.Cancel();
+                break;
+        }*/
+
+        if (pressed)
+        {
+            this.Exit();
+        }
+    }
+
+    public void Exit()
+    {
+        if (this.Destination != "")
+        {
+            //ProgressData.LoadFromDisk(true);
+            SceneManager.LoadScene(this.Destination);
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            Application.Quit();
+        }
+    }
+}
